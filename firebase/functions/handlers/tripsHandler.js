a120c42444c93160a0496e0d6dec6e5ff5c3428b
Avihai Hemo <208253369@STUD.colman.ac.il>
2021-03-05 14:38:22 +0200

    fix getTrips of user in firebase/triphandler

--- firebase/functions/handlers/tripsHandler.js
+++ firebase/functions/handlers/tripsHandler.js
@@ -4,68 +4,60 @@ exports.addTrip = (req,res) => {
     db.collection('trips')
 };
 
-exports.getTrips = (req,res) => {
-    let trips = [];
+exports.getTrips = async(req,res) => {
     let tripsIds = [];
-    db.doc(`users/${req.user.handle}`)
-    .get()
-    .then((doc) => {
-      if(doc.exists) {
-        if(doc.data().tripsRef) {
-          tripsIds = doc.data().tripsRef;
-          if(tripsIds.length > 0) {
-            let tripsProcessed = 0;
-            tripsIds.forEach((trip) => {
-              trip.get()
-              .then(tripData => {
-                tripsProcessed++;
-                let newTrip = tripData.data();
-                newTrip.id = tripData.id;
-                if(newTrip.sectionsRef) {
-                  let sectionsIds = newTrip.sectionsRef;
-                  if(sectionsIds.length > 0) {
-                    let newSections = [];
-                    sectionsIds.forEach(section => {
-                      section.get()
-                      .then(sectionData => {
-                        let newSection = sectionData.data();
-                        newSection.id = sectionData.id;
-                        if(newSection.accommodationRef){
-                          newSection.accommodationRef.get()
-                          .then(accommodation => {
-                            newSection.accommodationData = accommodation.data();
-                            if(newSection.accommodationRef) delete newSection.accommodationRef
-                            newSections.push(newSection);
-                          })
-                          .catch(err => console.error(err));
-                        } else {
-                          newSections.push(newSection);
-                        }
-                      })
-                      .catch(err => console.error(err));
-                    });
-                    newTrip.sectionsData = newSections;
-                  }
-                  delete newTrip.sectionsRef;
-                  if(newTrip.userRef) delete newTrip.userRef;
-                }
-                trips.push(newTrip);
-                if(tripsProcessed === tripsIds.length) {
-                    return res.json(trips);
-                }
-              })
-              .catch(err => console.error(err));
-            });
-  
-          }
-          else {
-            return res.status(404).json({error:"trips not found"});
+    let doc = await db.doc(`users/${req.user.handle}`).get();
+    if(doc.exists) {
+      if(doc.data().tripsRef) {
+        tripsIds = doc.data().tripsRef;
+        if(tripsIds.length > 0) {
+          let tripsPromises = []
+          tripsIds.forEach(tripId => {
+            tripsPromises.push(tripHelper(tripId));
+          });
+          return Promise.all(tripsPromises).then((trips) => {
+            return res.json(trips)
+          });
+        }
+        else {
+          return res.status(404).json({error:"trips not found"});
         }
         }
       } else {
         return res.status(404).json({error:"user not found"});
       }
-    //   return res.json(trips);
-    })
-    // .then((data) => {return res.json(data);})
+}
+
+tripHelper = async(tripId) => {
+  let tripData = await tripId.get();
+  let newTrip = tripData.data();
+  newTrip.id = tripData.id;
+  if(newTrip.sectionsRef) {
+    let sectionsIds = newTrip.sectionsRef;
+    if(sectionsIds.length > 0) {
+      let sectionsPromises = [];
+      sectionsIds.forEach(sectionId => {
+        sectionsPromises.push(sectionHelper(sectionId));
+      });
+      await Promise.all(sectionsPromises).then((newSections) => {
+        newTrip.sectionsData = newSections;
+      });
+      
+    }
+    delete newTrip.sectionsRef;
   }
+  if(newTrip.userRef) delete newTrip.userRef;
+  return newTrip;
+}
+
+sectionHelper = async(sectionId) => {
+  let sectionData = await sectionId.get();
+  let newSection = sectionData.data();
+  newSection.id = sectionData.id;
+  if(newSection.accommodationRef){
+    let accommodation = await newSection.accommodationRef.get();
+    newSection.accommodationData = accommodation.data();
+    if(newSection.accommodationRef) delete newSection.accommodationRef
+  }
+  return newSection;
+}
