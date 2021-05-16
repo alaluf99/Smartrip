c224b466063599bf9482b496f942ddf56849d0b2
Avihai Hemo <208253369@STUD.colman.ac.il>
2021-05-16 17:49:32 +0300

    update getTrips

--- Server/controllers/tripsController.js
+++ Server/controllers/tripsController.js
@@ -3,15 +3,12 @@ var tripsService = require('../services/tripsService');
 const tripsController = {
     async getTrips(req,res) {
         try{
-            var trips = await tripsService.getTrips(req.user.handle);
+            var trips = await tripsService.getTrips(req.user.email);
             return res.status(200).json({data:trips});
         } catch(e) {
             console.error(e);
             return res.status(400).json({error:e.message});
       }
-    },
-    async getTrip(req,res) {
-
     },
     async addNewTrip(req,res) {},
     async planTrip(req,res) {
--- Server/routes/tripsRouter.js
+++ Server/routes/tripsRouter.js
@@ -4,7 +4,6 @@ var tripsController = require('../controllers/tripsController');
 var fbAuth = require('../utils/fbAuth');
 
 tripsRouter.get('/', fbAuth, tripsController.getTrips);
-tripsRouter.get('/:id', fbAuth, tripsController.getTrip);
 tripsRouter.get('/plan', fbAuth, tripsController.planTrip);
 
 module.exports = tripsRouter;
