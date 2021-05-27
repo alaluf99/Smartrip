8caa145129e05896412f10835d6df415df76032f
Shahar Alaluf <alaluf99@gmail.com>
2021-05-27 20:47:08 +0300

    Add dockerfile to server

--- client/src/actions/planActions.js
+++ client/src/actions/planActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const planTrip = (plan) => {
-    axios.post('http://localhost:3000/api/trips/plan', {plan: plan})
+    axios.post(config.serverUrl + '/trips/plan', {plan: plan})
         .then((res) => {
             return res.data
         })
