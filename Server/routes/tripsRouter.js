188ff5bf69fd7880eb87c615e745c726b5b74062
Shahar Alaluf <alaluf99@gmail.com>
2021-06-05 19:55:20 +0300

    Add dockerfile to server

--- Server/routes/tripsRouter.js
+++ Server/routes/tripsRouter.js
@@ -4,7 +4,7 @@ var tripsController = require("../controllers/tripsController");
 var fbAuth = require("../utils/fbAuth");
 
 tripsRouter.get("/history", fbAuth, tripsController.getTrips);
-tripsRouter.get("/plan", fbAuth, tripsController.planTrip);
+tripsRouter.post("/plan", fbAuth, tripsController.planTrip);
 tripsRouter.get("/suggestions", fbAuth, tripsController.getSuggestions);
 
 module.exports = tripsRouter;
