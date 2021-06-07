e4d23cdddea19dadce21b533fc07b55d4f07d27b
galde135 <galde135@gmail.com>
2021-06-07 19:42:28 +0300

    getRequestDateFormat fix

--- client/src/features/plan/planning-form-page/add-location-modal.js
+++ client/src/features/plan/planning-form-page/add-location-modal.js
@@ -1,10 +1,45 @@
 import { Button, Checkbox, Grid, TextField } from "@material-ui/core";
 import { KeyboardDatePicker } from "@material-ui/pickers";
 import React, { useState } from "react";
-import Select from 'react-select';
+import Select from "react-select";
 
-const citiesList = ["Dubai", "London", "Amsterdam", "Sydney", "Dublin", "Singapore", "Milan", "Rotterdam", "Las Vegas", "Miami", "Quebec City", "Geneva", "Prague", "Nice", "Nairobi", "Tel Aviv", "Hamburg", "Venice", "Paphos City", "Cairo", "Athens", "Jaipur", "Munich", "Manchester", "Liverpool", "Toronto", "Vancouver", "Tokyo", "Seoul", "Rome"]
-const cities = citiesList.map(c => {return {value: c, label: c} })
+const citiesList = [
+  "Dubai",
+  "London",
+  "Amsterdam",
+  "Sydney",
+  "Dublin",
+  "Singapore",
+  "Milan",
+  "Rotterdam",
+  "Las Vegas",
+  "Miami",
+  "Quebec City",
+  "Geneva",
+  "Prague",
+  "Nice",
+  "Nairobi",
+  "Tel Aviv",
+  "Hamburg",
+  "Venice",
+  "Paphos City",
+  "Cairo",
+  "Athens",
+  "Jaipur",
+  "Munich",
+  "Manchester",
+  "Liverpool",
+  "Toronto",
+  "Vancouver",
+  "Tokyo",
+  "Seoul",
+  "Rome",
+  "Bucharest",
+  "Budapest",
+];
+const cities = citiesList.map((c) => {
+  return { value: c, label: c };
+});
 
 export default function AddLocationModal({ onAddLocation }) {
   const [location, setLocation] = useState();
@@ -14,22 +49,20 @@ export default function AddLocationModal({ onAddLocation }) {
   const [numberOfDays, setNumberOfDays] = useState(2);
 
   const setLoc = (e) => {
-    if(e) {
-      setLocation(e.value)
+    if (e) {
+      setLocation(e.value);
     }
-  }
+  };
 
   const getRequestDateFormat = (date) => {
     if (date) {
-      var date1 = new Date(date)
-      var day = date1.getDay();
-      var month = date1.getMonth();
-      var fullDay = day > 9 ? day : "0" + day.toString()
-      var fullMonth = month > 9 ? month : "0" + month.toString()
-      return date1.getFullYear() + "-" + fullDay + "-" + fullMonth
+      var date1 = new Date(date);
+      var day = ("0" + date1.getDate()).slice(-2);
+      var month = ("0" + (date1.getMonth() + 1)).slice(-2);
+      return date1.getFullYear() + "-" + month + "-" + day;
     }
-    return ""
-  }
+    return "";
+  };
 
   const addLocation = () => {
     const newLocation = {
@@ -44,79 +77,89 @@ export default function AddLocationModal({ onAddLocation }) {
       newLocation.endDate = getRequestDateFormat(endDate);
     }
 
-    onAddLocation(newLocation)
+    onAddLocation(newLocation);
     setLocation("");
-    setIsFlexible(true)
+    setIsFlexible(true);
     setNumberOfDays(2);
-  }
+  };
 
   return (
-      <Grid container>
-        <Grid item xs={4}>
-          <Select
-            className="basic-single"
-            classNamePrefix="select"
-            isClearable={true}
-            isSearchable={true}
-            name="city"
-            options={cities}
-            onChange={setLoc}
-          />
-        </Grid>
-        <Grid item xs={3}>
-          <Checkbox
-          color={"primary"}
-            checked={isFlexible}
-            onChange={e => setIsFlexible(e.target.checked)}
-            inputProps={{ 'aria-label': 'primary checkbox' }}
-          />Is flexible
-              </Grid>
-        <Grid item xs={3}>
-          {isFlexible ?
-            <TextField
-              type="number"
-              variant="outlined"
-              label="days number"
-              name="numberOfDays"
-              value={numberOfDays}
-              onChange={e => setNumberOfDays(e.target.value)} /> :         <Grid>
-              <KeyboardDatePicker
-                disableToolbar
-                name="datee"
-                variant="inline"
-                format="MM/dd/yyyy"
-                id="travel start date"
-                label="travel start date"
-                value={startDate}
-                onChange={(date) => {
-                  setStartDate(date);
-                }}
-                KeyboardButtonProps={{
-                  "aria-label": "change date",
-                }}
-                className="date-picker"
-              />
-    
-              <KeyboardDatePicker
-                disableToolbar
-                name="datee"
-                variant="inline"
-                format="MM/dd/yyyy"
-                margin="normal"
-                id="travel end date"
-                label="travel end date"
-                value={endDate}
-                onChange={(date) => {
-                  setEndDate(date);
-                }}
-                KeyboardButtonProps={{
-                  "aria-label": "change date",
-                }}
-                className="date-picker"
-              />
-            </Grid>}
-        </Grid>
-        <Grid item xs={2}> <Button color="primary" variant="outlined" onClick={addLocation}>Add</Button></Grid>
+    <Grid container>
+      <Grid item xs={4}>
+        <Select
+          className="basic-single"
+          classNamePrefix="select"
+          isClearable={true}
+          isSearchable={true}
+          name="city"
+          options={cities}
+          onChange={setLoc}
+        />
       </Grid>
+      <Grid item xs={3}>
+        <Checkbox
+          color={"primary"}
+          checked={isFlexible}
+          onChange={(e) => setIsFlexible(e.target.checked)}
+          inputProps={{ "aria-label": "primary checkbox" }}
+        />
+        Is flexible
+      </Grid>
+      <Grid item xs={3}>
+        {isFlexible ? (
+          <TextField
+            type="number"
+            variant="outlined"
+            label="days number"
+            name="numberOfDays"
+            value={numberOfDays}
+            onChange={(e) => setNumberOfDays(e.target.value)}
+          />
+        ) : (
+          <Grid>
+            <KeyboardDatePicker
+              disableToolbar
+              name="datee"
+              variant="inline"
+              format="MM/dd/yyyy"
+              id="travel start date"
+              label="travel start date"
+              value={startDate}
+              onChange={(date) => {
+                setStartDate(date);
+              }}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+              className="date-picker"
+            />
+
+            <KeyboardDatePicker
+              disableToolbar
+              name="datee"
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="travel end date"
+              label="travel end date"
+              value={endDate}
+              onChange={(date) => {
+                setEndDate(date);
+              }}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+              className="date-picker"
+            />
+          </Grid>
+        )}
+      </Grid>
+      <Grid item xs={2}>
+        {" "}
+        <Button color="primary" variant="outlined" onClick={addLocation}>
+          Add
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
